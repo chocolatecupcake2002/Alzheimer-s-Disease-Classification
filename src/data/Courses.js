eeb6c83c797025c2ac020a5c7aa9063c464271b5
Aishwarya <85008182+chocolatecupcake2002@users.noreply.github.com>
2024-05-13 17:59:43 +0530

    Add files via upload

---
+++ src/data/Courses.js
@@ -0,0 +1,57 @@
+export const courses = [
+    {
+      id: 1,
+      title: 'Alzheimers And Dementia',
+      category:'Article',
+      context: 'How and why signs of Dementia can indicate Alzheimers.',
+      linkImg:
+      'https://www.presbyterianhomes.org/app/uploads/526cebc8-shutterstock_1877389102-1550x1033.jpg',
+      Link: 'https://www.alz.org/alzheimers-dementia/difference-between-dementia-and-alzheimer-s',
+    },
+
+    {
+      id: 2,
+      title: 'Nutrition and Alzheimers',
+      category:'Article',
+      context:'Discover food and nutrition to benefit individuals with Alzheimers.',
+      linkImg:
+        'https://foodforthebrain.org/wp-content/uploads/2022/12/Brain-food-Food-for-the-Brain-Foundation.png',
+      Link: 'https://www.nia.nih.gov/health/alzheimers-and-dementia/what-do-we-know-about-diet-and-prevention-alzheimers-disease#:~:text=Similar%20to%20the%20Mediterranean%20diet,%2C%20and%20fast%2Ffried%20food.',
+    },
+    {
+      id: 3,
+      title: 'Latest Discoveries and Breakthroughs ',
+      category:'Article',
+      context:'Latest advancements and breakthroughs in Alzheimers research and treatment.',
+      linkImg:
+        'https://stevens.usc.edu/wp-content/uploads/2023/11/iStock-187272969.jpg',
+      Link: 'https://www.labiotech.eu/best-biotech/six-advancements-alzheimers-research-over-past-year/',
+    },
+    {
+      id: 4,
+      title: 'Early Detection of Dementia: Significance',
+      category:'Article',
+      context:'Importance of early detection and diagnosis in managing Alzheimers.',
+      linkImg:
+        'https://images.everydayhealth.com/images/news/brain-scan-early-detection-alzheimers-disease-1440x810.jpg?sfvrsn=2208476_5',
+      Link: 'https://www.mayoclinic.org/diseases-conditions/alzheimers-disease/symptoms-causes/syc-20350447',
+    },
+    {
+      id: 5,
+      title: 'Lifestyle Changes to prevent Alzheimers ',
+      category:'Article',
+      context:' lifestyle modifications and habits to help reduce the risk of Alzheimers.',
+      linkImg:
+        'https://www.baptisthealth.com/-/media/images/migrated/blog-images/teaser-images/lower-risk-of-alzheimers.jpg?rev=7f0d755cf95947e78db15a8e982f077d',
+      Link: 'https://www.nih.gov/news-events/news-releases/combination-healthy-lifestyle-traits-may-substantially-reduce-alzheimers',
+    },
+    {
+      id: 6,
+      title: 'Create A Dementia-Friendly Environment',
+      category:'Article',
+      context:'Discover tips for modifying home spaces for individuals with Alzheimers.',
+      linkImg:
+        'https://www.mymove.com/wp-content/uploads/2021/03/GettyImages-1092112400-scaled.jpg',
+      Link: 'https://www.nhs.uk/conditions/dementia/living-with-dementia/home-environment/#:~:text=Most%20people%20with%20dementia%2C%20and,(as%20much%20as%20possible).',
+    },
+  ];
