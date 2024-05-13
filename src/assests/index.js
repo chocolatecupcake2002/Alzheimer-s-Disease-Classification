db60395995f09bfea8517eb6f34a277c865c2997
Aishwarya <85008182+chocolatecupcake2002@users.noreply.github.com>
2024-05-13 17:57:15 +0530

    Add files via upload

---
+++ src/assests/index.js
@@ -0,0 +1,54 @@
+import hamburgerMenu from "./hamburgerMenu.svg";
+import close from "./close.svg";
+import logo from "./logo.png";
+import lock from "./lock.svg";
+import heroImg from "./heroImg.png";
+import companyLogo1 from "./company-logo-1.png";
+import companyLogo2 from "./company-logo-2.png";
+import companyLogo3 from "./company-logo-3.png";
+import companyLogo4 from "./company-logo-4.png";
+import blogImg1 from "./blog-1.png";
+import achievement from "./achievement.png";
+import avatar from "./avatar.png";
+import quotationMark from "./quotationMark.png";
+import cta from "./cta.png";
+import project from "./project.gif";
+import research from "./research.png";
+import brainy from "./brainy.png";
+import brainicon from "./brainicon.png";
+import brainicon2 from "./brainicon2.png";
+import health from "./health.png";
+import hand from "./hand.png";
+import pro from "./pro.png";
+import human from "./human.png";
+import doc from "./doc.png";
+
+
+export {
+  hamburgerMenu,
+  close,
+  logo,
+  lock,
+  heroImg,
+  companyLogo1,
+  companyLogo2,
+  companyLogo3,
+  companyLogo4,
+  blogImg1,
+  achievement,
+  avatar,
+  quotationMark,
+  cta,
+  project,
+  research,
+  brainy,
+  brainicon,
+  brainicon2,
+  health,
+  hand,
+  pro,
+  human,
+  doc
+
+
+};
